ee36d6c6e69e45bdf1575375fbcbfc096b6e6313
Abdullokh <giyasovabdullo0@gmail.com>
2022-08-19 17:29:59 +0500

    initial commit

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/full-mern-stack-video");
 
+
+app.get("/", (req,res)=>{
+  res.send("welcome to home")
+})
+
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
   res.send('HEllo world')
@@ -56,35 +61,35 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-app.get("/api/quote", async (req, res) => {
-  const token = req.headers["x-access-token"];
+// app.get("/api/quote", async (req, res) => {
+//   const token = req.headers["x-access-token"];
 
-  try {
-    const decoded = jwt.verify(token, "secret123");
-    const email = decoded.email;
-    const user = await User.findOne({ email: email });
+//   try {
+//     const decoded = jwt.verify(token, "secret123");
+//     const email = decoded.email;
+//     const user = await User.findOne({ email: email });
 
-    return res.json({ status: "ok", quote: user.quote });
-  } catch (error) {
-    console.log(error);
-    res.json({ status: "error", error: "invalid token" });
-  }
-});
+//     return res.json({ status: "ok", quote: user.quote });
+//   } catch (error) {
+//     console.log(error);
+//     res.json({ status: "error", error: "invalid token" });
+//   }
+// });
 
-app.post("/api/quote", async (req, res) => {
-  const token = req.headers["x-access-token"];
+// app.post("/api/quote", async (req, res) => {
+//   const token = req.headers["x-access-token"];
 
-  try {
-    const decoded = jwt.verify(token, "secret123");
-    const email = decoded.email;
-    await User.updateOne({ email: email }, { $set: { quote: req.body.quote } });
+//   try {
+//     const decoded = jwt.verify(token, "secret123");
+//     const email = decoded.email;
+//     await User.updateOne({ email: email }, { $set: { quote: req.body.quote } });
 
-    return res.json({ status: "ok" });
-  } catch (error) {
-    console.log(error);
-    res.json({ status: "error", error: "invalid token" });
-  }
-});
+//     return res.json({ status: "ok" });
+//   } catch (error) {
+//     console.log(error);
+//     res.json({ status: "error", error: "invalid token" });
+//   }
+// });
 
 app.listen(1337, () => {
   console.log("Server started on 1337");
